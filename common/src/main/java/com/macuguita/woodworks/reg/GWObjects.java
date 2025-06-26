35f7947ed77d1b8483eb7e81c681d1a001ade896
macuguita <raulpripri@gmail.com>
2025-06-26 06:19:53 +0200

    changed the order of create tabs back to normal

--- common/src/main/java/com/macuguita/woodworks/reg/GWObjects.java
+++ common/src/main/java/com/macuguita/woodworks/reg/GWObjects.java
@@ -45,26 +45,36 @@ public class GWObjects {
 	public static final GuitaRegistry<Block> STUMP_BLOCKS = GuitaRegistries.create(BLOCKS);
 	public static final GuitaRegistry<Block> STRIPPED_STUMP_BLOCKS = GuitaRegistries.create(BLOCKS);
 	public static final GuitaRegistry<Item> STUMP_ITEMS = GuitaRegistries.create(ITEMS);
-	public static final GuitaRegistryEntry<Block> OAK_STUMP = createStump("oak_stump", Blocks.OAK_WOOD);
-	public static final GuitaRegistryEntry<Block> SPRUCE_STUMP = createStump("spruce_stump", Blocks.SPRUCE_WOOD);
-	public static final GuitaRegistryEntry<Block> BIRCH_STUMP = createStump("birch_stump", Blocks.BIRCH_WOOD);
-	public static final GuitaRegistryEntry<Block> JUNGLE_STUMP = createStump("jungle_stump", Blocks.JUNGLE_WOOD);
-	public static final GuitaRegistryEntry<Block> ACACIA_STUMP = createStump("acacia_stump", Blocks.ACACIA_WOOD);
-	public static final GuitaRegistryEntry<Block> DARK_OAK_STUMP = createStump("dark_oak_stump", Blocks.DARK_OAK_WOOD);
-	public static final GuitaRegistryEntry<Block> MANGROVE_STUMP = createStump("mangrove_stump", Blocks.MANGROVE_WOOD);
-	public static final GuitaRegistryEntry<Block> CHERRY_STUMP = createStump("cherry_stump", Blocks.CHERRY_WOOD);
-	public static final GuitaRegistryEntry<Block> CRIMSON_STUMP = createStump("crimson_stump", Blocks.CRIMSON_HYPHAE);
-	public static final GuitaRegistryEntry<Block> WARPED_STUMP = createStump("warped_stump", Blocks.WARPED_HYPHAE);
 	public static final GuitaRegistry<Item> STRIPPED_STUMP_ITEMS = GuitaRegistries.create(ITEMS);
+
+	public static final GuitaRegistryEntry<Block> OAK_STUMP = createStump("oak_stump", Blocks.OAK_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_OAK_STUMP = createStrippedStump("stripped_oak_stump", Blocks.STRIPPED_OAK_WOOD);
+
+	public static final GuitaRegistryEntry<Block> SPRUCE_STUMP = createStump("spruce_stump", Blocks.SPRUCE_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_SPRUCE_STUMP = createStrippedStump("stripped_spruce_stump", Blocks.STRIPPED_SPRUCE_WOOD);
+
+	public static final GuitaRegistryEntry<Block> BIRCH_STUMP = createStump("birch_stump", Blocks.BIRCH_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_BIRCH_STUMP = createStrippedStump("stripped_birch_stump", Blocks.STRIPPED_BIRCH_WOOD);
+
+	public static final GuitaRegistryEntry<Block> JUNGLE_STUMP = createStump("jungle_stump", Blocks.JUNGLE_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_JUNGLE_STUMP = createStrippedStump("stripped_jungle_stump", Blocks.STRIPPED_JUNGLE_WOOD);
+
+	public static final GuitaRegistryEntry<Block> ACACIA_STUMP = createStump("acacia_stump", Blocks.ACACIA_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_ACACIA_STUMP = createStrippedStump("stripped_acacia_stump", Blocks.STRIPPED_ACACIA_WOOD);
+
+	public static final GuitaRegistryEntry<Block> DARK_OAK_STUMP = createStump("dark_oak_stump", Blocks.DARK_OAK_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_DARK_OAK_STUMP = createStrippedStump("stripped_dark_oak_stump", Blocks.STRIPPED_DARK_OAK_WOOD);
+
+	public static final GuitaRegistryEntry<Block> MANGROVE_STUMP = createStump("mangrove_stump", Blocks.MANGROVE_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_MANGROVE_STUMP = createStrippedStump("stripped_mangrove_stump", Blocks.STRIPPED_MANGROVE_WOOD);
+
+	public static final GuitaRegistryEntry<Block> CHERRY_STUMP = createStump("cherry_stump", Blocks.CHERRY_WOOD);
 	public static final GuitaRegistryEntry<Block> STRIPPED_CHERRY_STUMP = createStrippedStump("stripped_cherry_stump", Blocks.STRIPPED_CHERRY_WOOD);
+
+	public static final GuitaRegistryEntry<Block> CRIMSON_STUMP = createStump("crimson_stump", Blocks.CRIMSON_HYPHAE);
 	public static final GuitaRegistryEntry<Block> STRIPPED_CRIMSON_STUMP = createStrippedStump("stripped_crimson_stump", Blocks.STRIPPED_CRIMSON_HYPHAE);
+
+	public static final GuitaRegistryEntry<Block> WARPED_STUMP = createStump("warped_stump", Blocks.WARPED_HYPHAE);
 	public static final GuitaRegistryEntry<Block> STRIPPED_WARPED_STUMP = createStrippedStump("stripped_warped_stump", Blocks.STRIPPED_WARPED_HYPHAE);
 
 	public static GuitaRegistryEntry<Block> createStump(String name, Block wood) {
