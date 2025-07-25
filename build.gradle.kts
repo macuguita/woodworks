5a33d8890242246106ddc8bfada4c7a3f45ead03
macuguita <raulpripri@gmail.com>
2025-07-26 00:22:18 +0200

    build.gradle.kts: added automated publishing, removed github one

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
+import com.matthewprenger.cursegradle.CurseArtifact
+import com.matthewprenger.cursegradle.CurseProject
+import com.matthewprenger.cursegradle.CurseRelation
+import com.matthewprenger.cursegradle.Options
 import net.fabricmc.loom.api.LoomGradleExtensionAPI
 
 plugins {
     id("dev.architectury.loom") version "1.10-SNAPSHOT" apply false
     id("architectury-plugin") version "3.4-SNAPSHOT"
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
+    id("com.modrinth.minotaur") version "2.+"
+    id("com.matthewprenger.cursegradle") version "1.4.0"
 }
 
 architectury {
@@ -55,6 +61,8 @@ allprojects {
 
 subprojects {
     apply(plugin = "dev.architectury.loom")
+    apply(plugin = "com.modrinth.minotaur")
+    apply(plugin = "com.matthewprenger.cursegradle")
 
     val loom = project.extensions.getByType<LoomGradleExtensionAPI>()
 
@@ -69,6 +77,66 @@ subprojects {
                 mappings("dev.architectury:yarn-mappings-patch-neoforge:${BuildConfig.yarnMappingsNeoforgePatchVersion}")
             }
         )
+    }
+
+    var modLoader = name
+    val changelogText: String = File("CHANGELOG.md").readText()
+
+    modrinth {
+        token.set(System.getenv("MODRINTH_TOKEN"))
+        projectId.set("NTFyR6MX")
+        versionNumber.set(BuildConfig.modVersion + "-" + modLoader)
+        versionName.set("guita's Woodworks " + versionNumber.get())
+        versionType.set("release")
+        uploadFile.set(tasks.named("remapJar").get())
+        additionalFiles.add(tasks.named("remapSourcesJar").get())
+        changelog.set(changelogText)
+        gameVersions.addAll(BuildConfig.minecraftVersion)
+        if (modLoader == "fabric") {
+            loaders.addAll("fabric", "quilt")
+            dependencies {
+                required.project("fabric-api")
+            }
+        } else if (modLoader == "neoforge") {
+            loaders.add("neoforge")
+        }
+        required.project("macu-lib")
+    }
+
+    curseforge {
+        options(closureOf<Options> {
+            forgeGradleIntegration = false
+        })
+
+        project(closureOf<CurseProject> {
+            apiKey = System.getenv("CURSEFORGE_TOKEN")
+            id = "1308420"
+            releaseType = "release"
+            addGameVersion(BuildConfig.minecraftVersion)
+            if (modLoader == "fabric") {
+                addGameVersion("Fabric")
+                addGameVersion("Quilt")
+            } else if (modLoader == "neoforge") {
+                addGameVersion("NeoForge")
+            }
+            addGameVersion("Java 21")
+
+            changelogType = "markdown"
+            changelog = changelogText
+
+            mainArtifact(tasks.named("remapJar").get(), closureOf<CurseArtifact> {
+                displayName = "${BuildConfig.modId}-${BuildConfig.modVersion}-${modLoader}"
+            })
+
+            addArtifact(tasks.named("remapSourcesJar").get())
+
+            relations(closureOf<CurseRelation> {
+                if (modLoader == "fabric") {
+                    requiredDependency("fabric-api")
+                }
+                requiredDependency("macu-lib")
+            })
+        })
     }
 
     configure<JavaPluginExtension> {
