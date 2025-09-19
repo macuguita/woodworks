b10cb8d88bb5f2405d0a4f6e3db0c3542e38a44b
macuguita <raulpripri@gmail.com>
2025-09-19 21:20:39 +0200

    version bump

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -8,7 +8,7 @@ object BuildConfig {
     val architectureApiVersion: String = "9.2.14"
     val enabledPlatforms: String = "fabric,forge"
 
-    val modVersion: String = "1.0.2a-" + minecraftVersion
+    val modVersion: String = "1.1.0-" + minecraftVersion
     val mavenGroup: String = "com.macuguita.woodworks"
     val modId: String = "gwoodworks"
     val modName: String = "guita's Woodworks"
