4619bb6d2635a1e2b70a539658efa383d67f39b0
macuguita <raulpripri@gmail.com>
2026-02-21 18:57:56 +0100

    code reformat

--- common/src/main/java/com/macuguita/woodworks/mixin/FireBlockAccessor.java
+++ common/src/main/java/com/macuguita/woodworks/mixin/FireBlockAccessor.java
@@ -22,11 +22,11 @@
 
 package com.macuguita.woodworks.mixin;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Invoker;
-
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FireBlock;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(FireBlock.class)
 public interface FireBlockAccessor {
