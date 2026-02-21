2850b6f3383e93adf2224f260f2ecbdee904f0b3
macuguita <raulpripri@gmail.com>
2026-02-21 19:20:05 +0100

    dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,9 +128,12 @@ subprojects {
                     minecraftVersions.add(version)
                 if (modLoader == "fabric") {
                     requires("fabric-api")
+                    optional("modmenu")
                 }
                 requires("macu-lib")
                 optional("every-compat")
+                optional("mcqoy")
+                optional("qomc")
             }
             curseforge {
                 projectId = "1308420"
@@ -144,9 +147,12 @@ subprojects {
                 projectSlug = "guitas-woodworks"
                 if (modLoader == "fabric") {
                     requires("fabric-api")
+                    optional("modmenu")
                 }
                 requires("macu-lib")
                 optional("every-compat")
+                optional("mcqoy")
+                optional("qomc")
             }
         }
     }
